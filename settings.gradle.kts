39ba4e4244f940702f899233479e6ba60ffd17fe
way <way610@126.com>
2026-08-27 23:59:05 +0800

    Prefer official Maven repositories in CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,10 +26,10 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
     }
 }
 
